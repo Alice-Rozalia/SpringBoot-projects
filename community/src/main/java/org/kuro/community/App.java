810a904a4db1846851e7df249f127c566ccd56be
Sincerely <13557511525@139.com>
2021-01-28 10:26:40 +0800

    创建项目

---
+++ community/src/main/java/org/kuro/community/App.java
@@ -0,0 +1,17 @@
+package org.kuro.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 白鸟亦悲否？
+ * @Date: 2021/1/28 9:15
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
